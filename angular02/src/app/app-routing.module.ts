b489ff4302b2f12a8f3dc4d9a0250149843564ac
lucascagostinho <lucascagostinho@gmail.com>
2022-08-19 22:01:45 -0300

    Angular aula02
    Signed-off-by: lucascagostinho <lucascagostinho@gmail.com>

---
+++ angular02/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { OutroComponent } from './outro/outro.component';
+import { PaginaComponent } from './pagina/pagina.component';
+
+const routes: Routes = [
+  {path: "vaiPagina", component: PaginaComponent},
+  {path: "", component: InicioComponent},
+  {path: "outro", component: OutroComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
